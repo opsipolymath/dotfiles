de0fd357825fc6451f5c56ce9c893423090e20dc
opsipolymath <opsipolymath+github@gmail.com>
2023-07-06 01:41:07 -0400

    Fix grammar in comment header
    The comment header in this file was very unclear. This clarifies it and
    uses correct English.

--- .config/zsh/zmodules-available/99-auotfetch.zsh
+++ .config/zsh/zmodules-available/99-auotfetch.zsh
@@ -3,7 +3,8 @@
 # ${ZDOTDIR}/zmodules-enabled/99-autofetch.zsh
 #
 # Fetch changes to dotfiles silently in the background
-# NOTE: requires custom read-only deplay key exists
+# NOTE: Requires a custom read-only deploy key to exist
+#       at ${XDG_CONFIG_HOME}/ssh/opsipolymath-dotfiles.key
 
 if [[ -f '${XDG_CONFIG_HOME}/ssh/opsipolymath-dotfiles.key' ]]; then
 	GIT_SSH_COMMAND="${GIT_SSH_COMMAND} -i '${XDG_CONFIG_HOME}/ssh/opsipolymath-dotfiles.key'" git --git-dir="${XDG_CONFIG_HOME}/dotfiles" --work-tree="${HOME}" fetch &!
