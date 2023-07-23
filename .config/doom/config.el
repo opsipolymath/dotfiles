61554de2577de94cb1de07f17d1178c33ab94189
opsipolymath <opsipolymath+github@gmail.com>
2023-07-23 16:53:07 -0400

    Initial doom emacs config
    Setup name, email and font for emacs.

--- .config/doom/config.el
+++ .config/doom/config.el
@@ -6,8 +6,8 @@
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
-(setq user-full-name "John Doe"
-      user-mail-address "john@doe.com")
+(setq user-full-name "Darren Johnston"
+      user-mail-address "opsipolymath@gmail.com")
 
 ;; Doom exposes five (optional) variables for controlling fonts in Doom:
 ;;
@@ -28,6 +28,8 @@
 ;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
 ;; refresh your font settings. If Emacs still can't find your font, it likely
 ;; wasn't installed correctly. Font issues are rarely Doom issues!
+(setq doom-font (font-spec :family "JetBrainsMono Nerd Font" :size 14 :weight 'Regular)
+      doom-variable-pitch-font (font-spec :family "DejaVu Sans" :size 14))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
@@ -41,7 +43,7 @@
 
 ;; If you use `org' and don't want your org files in the default location below,
 ;; change `org-directory'. It must be set before org loads!
-(setq org-directory "~/org/")
+(setq org-directory "~/.config/emacs/org/")
 
 
 ;; Whenever you reconfigure a package, make sure to wrap your config in an
